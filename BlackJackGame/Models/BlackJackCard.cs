f95d16f8404e5b3c694b6784110fc12419fabd3d
Thomas Duchatelet <thomas.duchatelet.y3284@student.hogent.be>
2019-12-17 15:12:59 +0100

    Initial commit

---
+++ BlackJackGame/Models/BlackJackCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJackGame.Models
+{
+    public class BlackJackCard : Card
+    {
+
+        public bool FaceUp { get; set; }
+        public int Value { get; }
+
+
+    }
+}
