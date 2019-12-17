231bf6909a56e6d85e23595374dadcdf7d922296
Thomas Duchatelet <thomas.duchatelet.y3284@student.hogent.be>
2019-12-17 18:12:56 +0100

    Added constructor and methods to BlackJackCard

--- BlackJackGame/Models/BlackJackCard.cs
+++ BlackJackGame/Models/BlackJackCard.cs
@@ -6,10 +6,23 @@ namespace BlackJackGame.Models
 {
     public class BlackJackCard : Card
     {
-
+        #region Properties
         public bool FaceUp { get; set; }
         public int Value { get; }
+        #endregion
 
+        #region Constructors
+        public BlackJackCard(Suit suit, FaceValue faceValue) : base(suit,faceValue)
+        {
+            FaceUp = false;
+        }
+        #endregion
 
+        #region Methods
+        public void TurnCard()
+        {
+            FaceUp = !FaceUp;
+        }
+        #endregion
     }
 }
