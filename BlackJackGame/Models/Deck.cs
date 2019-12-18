5f0695595d7d5fabaada5bd937ed24e842c986b2
Thomas Duchatelet <thomas.duchatelet.y3284@student.hogent.be>
2019-12-18 14:23:05 +0100

    Implemented Deck Methods

--- BlackJackGame/Models/Deck.cs
+++ BlackJackGame/Models/Deck.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BlackJackGame.Models
@@ -8,18 +9,35 @@ namespace BlackJackGame.Models
     {
         #region Fields
         private IList<BlackJackCard> _cards;
-        private Random _random;
+        private Random _random = new Random();
         #endregion
 
+        #region Constructors
+        public Deck()
+        {
+            _cards = new List<BlackJackCard>();
+            foreach (var s in Enum.GetValues(typeof(Suit)))
+                foreach (var f in Enum.GetValues(typeof(FaceValue)))
+                    _cards.Add(new BlackJackCard((Suit)s, (FaceValue)f));
+            Shuffle();
+        }
+        #endregion
         #region Methods
         public BlackJackCard Draw()
         {
-            throw new NotImplementedException();
+            if (_cards.Count != 0)
+            {
+                BlackJackCard firstCard = _cards[0];
+                _cards.Remove(firstCard);
+                return firstCard;
+            }
+            else
+                throw new InvalidOperationException();
         }
 
         private void Shuffle()
         {
-            throw new NotImplementedException();
+            _cards = _cards.OrderBy(c => _random.Next()).ToList();
         }
         #endregion
     }
